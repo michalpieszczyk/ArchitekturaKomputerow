81a32c5a2e68776fd08bd8e72897a7967853fe97
Michał Pieszczyk <michalpieszczyk@gmail.com>
2019-03-31 12:56:28 +0200

    Add files via upload

---
+++ projekt.asm
@@ -0,0 +1,85 @@
+format PE Console 4.0
+
+entry start
+include 'win32a.inc'
+include 'win_macros.inc'
+
+section '.text' code readable executable
+start:
+	wyswietl txt
+	mov eax, 0
+wczyt_pierw:
+	pob_znak
+	cmp al, '0'
+	jb wczyt_pierw
+	cmp al, '1'
+	ja wczyt_pierw
+	wysw_znak al
+	mov [tab], al
+	jmp wczyt_drug
+
+
+wczyt_drug:
+	pob_znak
+	cmp al, 08h
+	je backspace1
+	cmp al, '0'
+	jb wczyt_drug
+	cmp al, '7'
+	ja wczyt_drug
+	wysw_znak al
+	mov [tab+1], al
+
+
+wczyt_trzec:
+	pob_znak
+	cmp al, 08h
+	je backspace2
+	cmp al, '0'
+	jb wczyt_drug
+	cmp al, '7'
+	ja wczyt_drug
+	wysw_znak al
+	mov [tab+2], al
+
+wyswietlanie:
+	wysw_znak ' '
+
+	mov bl, [tab]
+	wysw_znak bl
+	mov bl, [tab+1]
+	wysw_znak bl
+	mov bl, [tab+2]
+	wysw_znak bl
+
+	pob_znak
+	end_prog
+
+backspace1:
+	wysw_znak 08h
+	wysw_znak ' '
+	wysw_znak 08h
+	jmp wczyt_pierw
+
+backspace2:
+	wysw_znak 08h
+	wysw_znak ' '
+	wysw_znak 08h
+	jmp wczyt_drug
+
+backspace3:
+	wysw_znak 08h
+	wysw_znak ' '
+	wysw_znak 08h
+	jmp wczyt_trzec
+
+
+
+
+section '.data' data readable writeable
+	txt db 'Wprowadz wartosc osemkowa w kodzie U2 (100 000 - 77 777)',NULL
+	txt1 db 13,10,'wartosc BIN: ',NULL
+	txt2 db 13,10,'wartosc OCT: ',NULL
+	txt3 db 13,10,'wartosc DEC: ',NULL
+	txt4 db 13,10,'wartosc HEX: ',NULL
+	tab db 0, 0, 0, 0, 0, 0
