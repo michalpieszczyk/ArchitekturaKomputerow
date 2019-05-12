24b9fbfe0f94310ad4bf4365040cdc76b087bf25
Michał Pieszczyk <michalpieszczyk@gmail.com>
2019-05-12 16:09:24 +0200

    Add files via upload

--- Projekt2_Calkowanie.asm
+++ Projekt2_Calkowanie.asm
@@ -18,14 +18,12 @@ start:
 
 	wyswietl txt3
 	cinvoke scanf, <"%d">, zm3
-	; kolejna zmienna calkowita %d 
+       ; kolejna zmienna calkowita %d
 
 	finit
-	fld [zm2] ;pobierz zmienna zm1
-	fsub [zm1] 
-	;fld [zm2] ;pobierz zmienna zm2
-	;faddp ;dodaj
-	fstp [przed] ;zapisz st0 do wyn
+	fld [zm2]
+	fsub [zm1]
+	fstp [przed]
 
 	fld [przed]
 	fidiv [zm3]
@@ -50,8 +48,11 @@ zwieksz:
 	fld [obecna]
 	fadd [krok]
 	fstp [obecna]
+	;cinvoke printf, <'Obecna  = %.3lf ',0>, dword [obecna], dword [obecna+4]
+	;wysw_znak 13
+	;wysw_znak 10
 	fld [zm4]
-	fcom [obecna]
+	fcomp [obecna]
 	fstsw ax
 	sahf
 	jb dodane
@@ -63,11 +64,16 @@ sumuj:
 	fld [obecna]
 	fcos
 	fdiv [obecna]
+	;fstp [pom]
+	;fld [pom]
 	fadd [suma]
 	fstp [suma]
-	cinvoke printf, <'Suma  = %.3lf ',0>, dword [suma], dword [suma+4]
-	wysw_znak 13
-	wysw_znak 10
+	;cinvoke printf, <'Pomocnicza  = %.3lf ',0>, dword [pom], dword [pom+4]
+	;wysw_znak 13
+	;wysw_znak 10
+	;cinvoke printf, <'Suma  = %.3lf ',0>, dword [suma], dword [suma+4]
+	;wysw_znak 13
+	;wysw_znak 10
 	jmp zwieksz
 
 
@@ -86,11 +92,6 @@ dodane:
 	fadd [suma]
 	fmul [krok]
 	fstp [wyn]
-	
-
-;kod policzenia przedzialu i kroku, wartosci w funkcji
-;wynik do zmienej typu double i wyswietlic w taki sposób jak poniżej na 2x 32 bit
-;wyswietlenie wyniku - spos�b 2
 
 
 	wysw_znak 13
@@ -99,7 +100,6 @@ dodane:
 
 	;dzielimy na dwie czesci 64 bit / 2 = 32 bit na kazde wyswietlenie
 
-; zako�czenie programu lub powr�t
 koniec:
 	pob_znak
 
@@ -112,14 +112,13 @@ section '.data' data readable writeable
 	zm2  dq 0
 	zm3  dw 0
 	zm4  dq 0
+	pom  dq 0
 	przed dq 0
 	wyn  dq 0
 	krok dq 0
 	obecna dq 0
 	suma dq 0
-	dwa  dw 2 
-	jeden dw 1
-	zero dw 0
+	dwa  dw 2
 	dz   dw 10
 
 
@@ -127,4 +126,3 @@ section '.data' data readable writeable
 	txt1 db 'Podaj poczatek przedzialu calkowania (a): ',0
 	txt2 db 'Podaj koniec przedzialu calkowania (b): ',0
 	txt3 db 'Podaj ilosc krokow (n): ',0
-
